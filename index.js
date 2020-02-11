f6415c61767f9f73b4ad332e929ab92244c206dd
sammtcbn <sammtcbn@gmail.com>
2020-02-11 14:46:30 +0800

    different output file path for win and linux

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var interval = 1000;
-var outfile = '/tmp/tsdump.log';
+
+if (process.platform == 'win32') {
+    var outfile = 'tsdump.log';
+} else if (process.platform == 'linux') {
+    var outfile = '/tmp/tsdump.log';
+}
 
 var fs = require('fs');
  
@@ -10,7 +15,7 @@ function intervalFunc() {
     if (err)
         console.log(err);
     else
-        console.log('Write operation complete.');
+        console.log('write ' + timestamp);
     });
 }
 setInterval (intervalFunc, interval);
