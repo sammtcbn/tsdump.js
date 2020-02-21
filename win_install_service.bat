9087672e29f23882de57452a2087de3e866f0948
sammtcbn <sammtcbn@gmail.com>
2020-02-21 17:44:18 +0800

    fix AppDirectory have space characters

--- win_install_service.bat
+++ win_install_service.bat
@@ -1,5 +1,6 @@
 @set currdir=%~dp0
+IF "%currdir:~-1%"=="\" SET currdir=%currdir:~0,-1%
 nssm install tsdump "node"
-nssm set tsdump AppDirectory "%currdir%
+nssm set tsdump AppDirectory "%currdir%"
 nssm set tsdump AppParameters index.js
 nssm start tsdump
