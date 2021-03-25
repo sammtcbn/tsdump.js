af1fcbc24e150a0203210551f5444b78bccc2e6a
sammtcbn <sammtcbn@gmail.com>
2021-03-26 00:49:58 +0800

    minor update

--- linux_install.bash
+++ linux_install.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 SERVICE_NAME=tsdump.service
+INS_PATH=/usr/local/sammtcbn/tsdump
 
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
 
-mkdir -p /usr/local/sammtcbn/tsdump || exit 1
-cp -a -f index.js /usr/local/sammtcbn/tsdump || exit 1
-cp -a -f tsdump.service /etc/systemd/system || exit 1
+mkdir -p ${INS_PATH} || exit 1
+cp -a -f index.js ${INS_PATH} || exit 1
+cp -a -f ${SERVICE_NAME} /etc/systemd/system || exit 1
 
 systemctl daemon-reload || exit 1
 chmod 664 /etc/systemd/system/${SERVICE_NAME} || exit 1
--- linux_uninstall.bash
+++ linux_uninstall.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 SERVICE_NAME=tsdump.service
+INS_PATH=/usr/local/sammtcbn/tsdump
 
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
@@ -13,4 +14,4 @@ if [ -e /etc/systemd/system/${SERVICE_NAME} ]; then
     rm -f --preserve-root /etc/systemd/system/${SERVICE_NAME}
 fi
 
-rm -rf --preserve-root /usr/local/sammtcbn/tsdump
+rm -rf --preserve-root ${INS_PATH}
